900c9dca9d44aed152d5794d43dc76ebfabb1a31
Shelby Myrman <myrmanshelby@gmail.com>
2024-09-09 15:17:26 -0700

    Added skills section

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,18 @@
+import styles from "./Skills.module.css";
+import { getImageUrl } from "../../utils";
+import skills from "../../data/skills.json"
+
+export const Skills = () => {
+    return <section className={styles.container} id="skills">
+        <h2 className={styles.title}>Skills</h2>
+        <div className={styles.skills}>{
+            skills.map((skill, id) => {
+                return <div key={id} className={styles.skill}>
+                        <div className={styles.skillImageContainer}><img src={getImageUrl(skill.imageSrc)} alt={skill.title}/></div>
+                        <p>{skill.title}</p>
+                    </div>
+                }
+            )} 
+        </div>
+    </section>
+}
