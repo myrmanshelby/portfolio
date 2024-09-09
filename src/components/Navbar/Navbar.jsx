e13c33eddee327065bdd5172868b0e84dca9d69a
Shelby Myrman <myrmanshelby@gmail.com>
2024-09-08 22:17:39 -0700

    Added basic nav bar
    Added nav bar with styling - need to make it responsive

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Navbar.module.css"
+
+export const Navbar = () => {
+    return <nav className={styles.navbar}>
+        <a className={styles.title} href="/">Shelby Myrman</a>
+        <div className={styles.menu}>
+            <ul className={styles.menuItems}>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#my-story">My Story</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>;
+}
