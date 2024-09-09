a3c32bc7cca2c8e46bf24cfd85c8bc2db2bdcd17
Shelby Myrman <myrmanshelby@gmail.com>
2024-09-08 22:52:31 -0700

    Made nav bar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-import styles from "./Navbar.module.css"
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">Shelby Myrman</a>
         <div className={styles.menu}>
-            <ul className={styles.menuItems}>
+            <img className={styles.menuBtn} 
+                src={
+                menuOpen 
+                ? getImageUrl("nav/closeIcon.png") 
+                : getImageUrl("nav/menuIcon.png")
+                } 
+                alt="menu-button"
+                onClick={() => setMenuOpen(!menuOpen)}  
+            />
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}
+            >
                 <li><a href="#about">About</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Projects</a></li>
