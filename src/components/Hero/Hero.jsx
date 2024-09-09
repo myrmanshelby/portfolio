a5e7cac98f5e8a517d04550306c4020961787ebb
Shelby Myrman <myrmanshelby@gmail.com>
2024-09-09 10:00:25 -0700

    Completed hero section/added mobile responsive
    TODO: Add social icons and change text

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css"
+
+export const Hero = () => {
+    return <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'm Shelby</h1>
+            <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur 
+            adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna.</p>
+            <a href="#" className={styles.resumeBtn}>Resume</a> {/*TODO: Add other buttons and resume download*/}
+        </div>
+        <img src={getImageUrl("hero/heroImage.png")} alt="cartoon hero image of woman coding" className={styles.heroImg}/>
+        <div className={styles.topBlur}></div>
+        <div className={styles.bottomBlur}></div>
+    </section>
+}
